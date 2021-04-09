038fe9c8a24bf272db1ef219496cb32974f7b659
choton654 <choton654@gmail.com>
2021-04-09 21:49:42 +0530

    ..

--- src/api.js
+++ src/api.js
@@ -1,3 +1,3 @@
-// const BASE_URL = `https://business-app-server-fasp3.ondigitalocean.app`;
-const BASE_URL = `http://localhost:8000`;
+const BASE_URL = `https://business-app-server-fasp3.ondigitalocean.app`;
+// const BASE_URL = `http://localhost:8000`;
 module.exports = BASE_URL;
